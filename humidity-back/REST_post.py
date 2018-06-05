44e7830d173375a7a0075f10b4bfaab098b11079
apple <wharasymwoicz@interia.eu>
2018-06-05 11:39:57 +0200

    modified:   humidity-back/REST_post.py 	modified:   humidity-back/dht11_main.py

--- humidity-back/REST_post.py
+++ humidity-back/REST_post.py
@@ -6,14 +6,52 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# base settings
+User = 'harnas'
+Passw = 'wojciech'
+Hostt = '51.15.87.74'
+dtbs = 'humidity'
+port = '5432'
+
 @app.route('/humidity', methods=['POST'])
 
 def humidity_post():
     print (request.is_json)
     content = request.get_json()
     print (content)
+    content_send(content)
 
-app.run(host='51.15.87.74', port= 5432)
+def connector():
+    global User, Passw, Hostt, dtbs, base, connect
+    try:
+        connect = pymysql.connect(host=Hostt, user=User, password=Passw, db=dtbs)
+        base = connect.cursor
+    except:
+        print("Error database connection !")
+        sleep(1)
+        connector()
+
+def content_send(content_name):
+	global base
+
+	if content_name == "":
+		base.execute("INSERT ")
+
+	elif content_name == "":
+		base.execute("")
+
+	elif content_name == "":
+		base.execute("")
+
+
+
+if __name__ is '__main__':
+	try:
+        connect = pymysql.connect(host=Hostt, user=User, password=Passw, db=dtbs)
+        base = connect.cursor
+    except:
+		connector()
+	app.run(host='51.15.87.74', port= 5432)
 
 
 
--- humidity-back/dht11_main.py
+++ humidity-back/dht11_main.py
@@ -3,22 +3,22 @@ from __future__ import unicode_literals
 import dht11
 
 def main():
-	global humidity, temperature
-	Connector()
-	while True:
-		zero()
-    	result = instance.read()
-    	if result.is_valid():
-        	print("Last valid input: " + str(datetime.datetime.now()))
-        	print("Temperature: %d C" % result.temperature)
-        	print("Humidity: %d %%" % result.humidity)
-        	humidity = float(result.humidity)
-        	temperature = float(result.temperature)
-        	Send_post(humidity, temperature)
-    	time.sleep(1)
+    global humidity, temperature
+    Connector()
+    while True:
+        zero()
+        result = instance.read()
+        if result.is_valid():
+            print("Last valid input: " + str(datetime.datetime.now()))
+            print("Temperature: %d C" % result.temperature)
+            print("Humidity: %d %%" % result.humidity)
+            humidity = float(result.humidity)
+            temperature = float(result.temperature)
+            Send_post(humidity, temperature)
+        time.sleep(1)
 
 if __name__ ==  "__main__":
-	zero()
-	main()
+    zero()
+    main()
 
 
