9d36a09ac02087c18bc2f1f123c4ec73795172c1
Rafał Ileczko <rileczko@gmail.com>
2018-06-06 07:56:40 +0200

    First working version

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const db_con_string = `tcp://${c.db.username}:${c.db.password}@${c.db.host}:${c.
 const postgre = new pg.Client(db_con_string);
 
 postgre.connect();
-// const query = postgre.query("INSERT INTO places(building, floor, room) values (1, 1, 'kuchnia')");
+let query = postgre.query('LISTEN new_data');
 
 // Express routes and statics setup
 app.use('/statics', express.static(__dirname + '/statics'));
@@ -24,8 +24,40 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', client => {
-  postgre.query("SELECT * FROM places", (err, data) => {
-    client.emit('connected', data.rows);
+  postgre.query(
+    `SELECT * FROM sensors
+     INNER JOIN places ON sensors.id_place=places.id_place
+     INNER JOIN measurements ON measurements.id_sensor=sensors.id_sensor
+     ORDER BY measurements.measured_at ASC
+     LIMIT 100`,
+    (err, data) => {
+      if(data.rows.length > 0) {
+        _data = Array.from(data.rows).reduce((result, obj) => {
+            const oid = obj['id_sensor'];
+            let measurement = {id_measurement: obj.id_measurement,
+                               temperature: obj.temperature,
+                               humidity: obj.humidity,
+                               measured_at: obj.measured_at};
+
+            if(!result[oid]) {
+              result[oid] = {id_sensor: obj.id_sensor,
+                             id_place: obj.id_place,
+                             building: obj.building,
+                             room: obj.room,
+                             measurements: [measurement]};
+            } else {
+              result[oid].measurements.push(measurement);
+            }
+            return result;
+        }, {});
+        client.emit('connected', _data);
+      }
+    });
+
+  postgre.on('notification', function(new_data) {
+    client.emit('new_data', JSON.parse(new_data.payload));
+  });
+  client.on('ready for data', function (data) {
   });
 
   client.on('event', function(data){});
@@ -34,3 +66,11 @@ io.on('connection', client => {
 
 
 server.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+
+// const query = postgre.query("INSERT INTO places(building, floor, room) values (1, 1, 'kuchnia')");
+// const query = postgre.query(`INSERT INTO measurements(id_sensor, temperature, humidity, timestamp)
+//                                                      values (1, 10.0, 12.0, ${new Date().valueOf()})`);
+// console.log(new Date().valueOf());
+// postgre.query("SELECT * FROM measurements", (err, data) => {
+//   console.log(data.rows);
+// });
--- statics/js/script.js
+++ statics/js/script.js
@@ -1,19 +1,69 @@
 (function() {
   'use strict';
 
+  let sensors = {};
   const socket = io.connect('http://' + document.domain + ':' + location.port);
 
   socket.on('connected', function(data) {
-    const html_string = data.reduce((result, data) => {
-      const string = `
-      <div class="sensor_wrapper">
-        <h2>Czujnik: ${data.room}</h2>
-        <div id="chart${data.id_place}"></div>
-      </div>`.replace(/(^[\n\r\s]+)/g, "").replace(/>[\n\r\s]+</g, "><");
-      return result + string;
-    },"");
-    $('#app').append(html_string);
+    Object.keys(data).forEach(key => {
+      const sensor = data[key];
+      const html_string = `
+        <div class="sensor_wrapper">
+          <h2>Czujnik: ${sensor.room}</h2>
+          <canvas id="chart${sensor.id_sensor}"></div>
+        </div>`.replace(/(^[\n\r\s]+)/g, "").replace(/>[\n\r\s]+</g, "><");
+      $('#app').append(html_string);
+
+      sensors[sensor.id_sensor] = Object.assign({}, sensor,{chart: createChart(sensor)});
+    });
   });
 
+  socket.on('new_data', function(data) {
+    console.log(data);
+    const chart = sensors[data.id_sensor].chart;
+
+    chart.data.labels.push(data.measured_at);
+    chart.data.datasets.forEach(dataset => {
+      dataset.data.push(data[dataset.id]);
+    });
+    chart.update();
+  });
 })();
 
+
+function createChart(sensor) {
+  const ctx = document.getElementById(`chart${sensor.id_sensor}`).getContext('2d');
+  return myLineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: sensor.measurements.map(item => parseInt(item.measured_at)),
+        datasets: [{
+          id: 'temperature',
+          label: "Temperatura",
+          data: sensor.measurements.map(item => item.temperature),
+          borderColor: 'red',
+          backgroundColor: 'transparent',
+          fill: false
+        }, {
+          id: 'humidity',
+          label: "Wilgotność",
+          data: sensor.measurements.map(item => item.humidity),
+          borderColor: 'blue',
+          backgroundColor: 'transparent',
+          fill: false
+        }]
+      },
+      options: {
+        scales: {
+          xAxes: [{
+            type: 'time',
+            time: {
+             displayFormats: {
+                 'millisecond': 'hh:ss:SSS',
+              }
+            }
+          }]
+        }
+      }
+  });
+}
